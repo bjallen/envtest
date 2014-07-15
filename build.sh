85f57db490a49931bc7cdf9c8e56da6154f631dd
B.J. Allen <bjallen@users.noreply.github.com>
2014-07-15 13:29:34 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+echo `ruby -v`
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]]
+ then
+   source "$HOME/.rvm/scripts/rvm"
+   export RVM_RUBY=$(cat .ruby-version)
+   rvm use $RVM_RUBY
+fi
+ 
+echo `ruby -v`
+
 echo $BAMBOO_BRANCH
